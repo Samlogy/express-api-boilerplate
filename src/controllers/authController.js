e4923fa1ab5b3d551c84659133caa78f18ffffdd
Axibord <ghiles.aitlounis@gmail.com>
2020-09-12 03:33:47 +0100

    added authorization signup token with JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,24 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 exports.signup = async (req, res, next) => {
 	try {
-		const newUser = await User.create(req.body)
+		const newUser = await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			passwordConfirm: req.body.password
+		})
+
+		const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+			expiresIn: process.env.JWT_EXPIRES_IN
+		})
 		res.status(201).json({
 			status: 'success',
-			data: newUser
+			token: token,
+			data: {
+				user: newUser
+			}
 		})
 	} catch (err) {
 		next(err)
