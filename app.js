28d6369edadba62a8a1f82d4b0b3a1711408b8e0
Axibord <ghiles.aitlounis@gmail.com>
2020-09-25 10:12:01 +0100

    added sanitization to prevent noSQL injec and XSS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// 3rd party modules
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xssCleaner = require('xss-clean')
+
+//own imports
 const contactsRouter = require('./src/routes/contactsRoutes')
 const usersRouter = require('./src/routes/usersRoutes')
 const AppError = require('./src/utils/appError')
@@ -35,9 +40,13 @@ app.use('/api', limiter)
 // serving/use static files
 app.use(express.static('folder-name'))
 
-// parse  json format in the body
-app.use(express.json())
+// parse body json format in the body
+app.use(express.json({ limit: '10kb' }))
 
+// data sanitization against NoSQL injection
+app.use(mongoSanitize())
+// data sanitization against XSS to prevent HTML code inside database
+app.use(xssCleaner())
 //-------------------------------------------------------------------------------------------
 //ROUTES
 app.use('/api/v1/contacts', contactsRouter)
